39b6b8762cb7c4726b0613e734edce5b5a338e8c
sollan0506 <66993956+sollan0506@users.noreply.github.com>
2020-08-15 12:12:00 +0900

    skill 부분 삭제
    skill 부분 삭제

--- js/main.js
+++ js/main.js
@@ -44,33 +44,6 @@ $(function(){
       $(this).css('background-color', 'rgba(0,0,0,0)');
       $(this).children('i').css('color', '#fff');
   });
-
-
-  // ScrollMagic Call
-  var controller = new ScrollMagic.Controller();
-
-  // 장면(Scene) 설정시작
-  var pb1 = $('.progress-bar1'),
-       pb2 = $('.progress-bar2'),
-       pb3 = $('.progress-bar3'),
-       pb4 = $('.progress-bar4'),
-       pb5 = $('.progress-bar5'),
-       pb6 = $('.progress-bar6'),
-       tl1 = new TimelineMax();
-
-  tl1.fromTo(pb1, 1, {width: 0}, {width: "70%"}, 0.5)
-    .fromTo(pb2, 1, {width: 0}, {width: "60%"}, 0.5)
-    .fromTo(pb3, 1, {width: 0}, {width: "90%"}, 0.5)
-    .fromTo(pb4, 1, {width: 0}, {width: "85%"}, 0.5)
-    .fromTo(pb5, 1, {width: 0}, {width: "75%"}, 0.5)
-    .fromTo(pb6, 1, {width: 0}, {width: "80%"}, 0.5);
-
-  var scene2 = new ScrollMagic.Scene({
-    triggerElement: '#skill',  
-    triggerHook: "0.9"
-  })
-  .setTween(tl1)
-  .addTo(controller);
   
 
 
